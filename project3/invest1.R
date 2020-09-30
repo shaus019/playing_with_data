30d6ad5b1596b544dea05bbd4218a76b1915df3c
Usman Shah <shaus019@student.otago.ac.nz>
2020-09-30 19:02:41 +1300

    have visualized the correlation table, to make it easy to understand

--- project3/invest1.R
+++ project3/invest1.R
@@ -2,3 +2,12 @@ invest <-read.table("invest.tab")
 head(invest)
 plot(invest)
 plot(invest$ROI,invest$Risk, xlab = "ROI",ylab = "Risk",main = "Plot for Risk over ROI",col="Blue")
+
+correlation <- read.table("corr(1).tab") # load the correaltion data
+head(correlation)
+boxplot(correlation)
+library(MASS)
+library(corrplot)
+
+correlationPlot <- cor(correlation)
+corrplot(correlationPlot)
